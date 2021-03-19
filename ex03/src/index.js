c9ab8d73c212972a34cf09a51beed8ac4c692379
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-19 10:23:14 +0100

    my pets

---
+++ ex03/src/index.js
@@ -0,0 +1,21 @@
+// Create myPetsArray array here
+myPetsArray = ["Dog", "Cat"];
+// Create of myPetsArray array
+function myArrayFunction(myPets) {
+    var myNewPets = (["Bird"], ["Fish"]);
+    // Only change code below this line
+
+    var FirstPet = ["Bird"];
+    var LastPet = ["Fish"];
+    myNewPets.unshift("Lion",);
+
+    myPetsArray.shift("Cat");
+    myNewPets.push("Bird");
+
+    LastPet.shift();
+
+    return myNewPets;
+    
+}
+console.log(myArrayFunction());
+module.exports = myArrayFunction;
