06bc4266825ebf00c9e6cff6c8cc4bc77a77fdaf
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-20 18:55:52 +0100

    finnished finnaly

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,23 +1,17 @@
-
-arr1 = ["a", "b", "c", "d" ]
-arr2 = ["Dell", "Philips", "Samsung", "AOX", "Sony"];
-
-function mySplice (arr1, arr2, n) {
-    return arr2.splice(n, 0, arr1); 
-   
-}
-   var i = 0;
-while (i < arr1.length) {
-    var x = [1, 2, 3, 4, 5];
-    if (arr1[i].test == x) {
-       arr2.splice(i, 1)
-    } else {
-       ++i;
+// Only change code below this line
+function mySplice(arr1, arr2, n) {
+    let combinedArrays = arr2.slice();
+    var temp = arr1.reverse();
+    for (let i = 0; i < temp.length; i++) {
+      combinedArrays.splice(n, 0, temp[i]);
+      n++;
     }
-
-
-console.log(mySplice([1, 2, 3], [4, 5], 1));
-console.log(mySplice([1, 2, 3], [4, 5], 2));
-console.log(mySplice(["b", "c"], ["a", "d"], 1));
-module.exports = mySplice.splice;
-}
+    return combinedArrays;
+  }
+  mySplice([1, 2, 3], [4, 5], 1);
+  // Only change code above this line
+  console.log(mySplice([1, 2, 3], [4, 5], 1));
+  console.log(mySplice([1, 2, 3], [4, 5], 2));
+  console.log(mySplice(["b", "c"], ["a", "d"], 1));
+  console.log(mySplice(["Dell", "Philips"], ["Samsung", "AOX", "Sony"], 2)); // Change this line
+  module.exports = mySplice;
