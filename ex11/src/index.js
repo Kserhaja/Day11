7a91ddeed53be9c23d26d9d94918f18fe60b9e3c
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-21 14:36:01 +0100

    sredjeno ponovo

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -1,16 +1,23 @@
 // Only change code below this line
 function splitArrayInGroups(arr, n) {
-    result=[];
-    size = n;
-    for (var n = 0; n = arr.length; n += size) {
-        result.push(arr.slice(n, size +n));
-      }
-      return result;
-    // Only change code above this line
+  var result = [];
+  for (var i = 0; i < arr.length; i += n) {
+    result.push(arr.slice(i, i + n));
+  }
+  return result.slice(0, n);
 }
-console.log(splitArrayInGroups(["a", "b", "c", "d"], 2)); // Change this line
+splitArrayInGroups(["a", "b", "c", "d"], 2);
+// Only change code above this line
+console.log(splitArrayInGroups(["a", "b", "c", "d"], 2));
 console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
 console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5], 2));
 console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 3));
 console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
+// Change this line
 module.exports = splitArrayInGroups;
+
+
+
+
+
+
